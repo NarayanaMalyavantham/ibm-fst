c57035699593016469460b2d689d44f14c878fa4
NarayanaMalyavantham <165993228+NarayanaMalyavantham@users.noreply.github.com>
2024-05-14 14:52:30 +0530

    Delete Selenium/test/java directory

--- Selenium/test/java/demo2.java
+++
@@ -1,8 +0,0 @@
-package suiteExample;
-import org.testng.annotations.Test;
-public class demo2 {
-    @Test
-    public void TestCase() {
-        System.out.println("I'm in the test case from DemoTwo Class");
-    }
-}
